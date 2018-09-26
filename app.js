d23e3a2372b3932e841cdfc54f528a88863bc238
Gia Cheng <jcheng.4869@gmail.com>
2018-09-26 14:04:14 +0800

    checkpoint 5 completed

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ function onReady() {
 
     // get the text
     let title = newToDoText.value;
+    //let todoObj = {
+    //  name: title,
+    //  id: todoId
+  //  }
+    //todoId++;
+    //toDos.push(todoObj);
     // create a new li
     let newLi = document.createElement('li');
 
@@ -16,19 +22,30 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    //set delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<span> Delete </span>';;
+    deleteBtn.addEventListener('click',function(){
+    toDoList.removeChild(this. parentElement);
+  })
+    //let buttonLiText = this.parentElement.childNodes[0].textContent;
+    //toDoList.removeChild(this.parentElement) ;
+
+
     // set the title
    newLi.textContent = title;
 
    // attach the checkbox to the li
    newLi.appendChild(checkbox);
+   newLi.appendChild(deleteBtn);
    // attach the li to the ul
    toDoList.appendChild(newLi);
    //empty the input
    newToDoText.value = '';
+});
 
-  });
 
-}
+};
 window.onload = function() {
   onReady();
 };
